1263287fc569897f201fcb9173dd880ddd929b88
Bhargavi <bhargavimandala@gmail.com>
2024-03-06 12:11:52 +0000

    added apply

--- provider.tf
+++ provider.tf
@@ -1,7 +1,15 @@
 provider "aws" {
   region  = "eu-west-2"
-  profile = "DevOps"
+  #profile = "DevOps"
+}
 
+resource "aws_s3_bucket" "s3" {
+  bucket = "github-actions-s3-mb-2"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
 }
 
 
